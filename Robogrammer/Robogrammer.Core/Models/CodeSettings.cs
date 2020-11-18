92a1b18a13fed81c383a34ac6660dc3dd385dc67
Adam Bajguz <adam.bajguz@gmail.com>
2020-11-18 18:39:22 +0100

    Fixes

--- Robogrammer/Robogrammer.Core/Models/CodeSettings.cs
+++ Robogrammer/Robogrammer.Core/Models/CodeSettings.cs
@@ -3,8 +3,6 @@
 
     public sealed class CodeSettings
     {
-        public string? Name { get; set; }
-
         public Ports LeftEnginePort { get; set; }
         public Ports RightEnginePort { get; set; }
     }
